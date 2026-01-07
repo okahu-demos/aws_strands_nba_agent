cca6e67eb979f3c946dbfeed16f9cc4603d9c31a
Ashokan <ashokan.perumal@okahu.ai>
2026-01-07 19:05:20 +0530

    Update Monocle apptrace version and workflow name for consistency

--- nba_agent.py
+++ nba_agent.py
@@ -1,6 +1,6 @@
 # Enable Monocle Tracing
 from monocle_apptrace import setup_monocle_telemetry
-setup_monocle_telemetry(workflow_name = 'aws_strands_nba_agent', monocle_exporters_list = 'file')
+setup_monocle_telemetry(workflow_name = 'okahu_demos_aws_strands_nba_agent', monocle_exporters_list = 'file')
 
 from datetime import datetime, timedelta
 import json
